231b96bf61842283fe595032db860850e2136d7f
Cristian-Mathias <cristian11mathias@hotmail.com>
2025-07-25 19:50:33 -0300

    implementa função para adicionar amigos com validação e atualização do array

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 let amigos = [];
 
+function adicionarAmigo() {
+    let nome = document.getElementById("amigo").value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]{3,}$/;
+
+    if (!regex.test(nome)) {
+        alert("Por favor, insira um nome válido (mínimo 3 letras, apenas letras e espaços).");
+        document.getElementById("amigo").value = "";
+        return; 
+    }
+
+    amigos.push(nome);
+    document.getElementById("amigo").value = "";
+}
